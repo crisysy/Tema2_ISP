5fc597eaa9504092a11074120916a35cf240cf6a
Cristina <cristinapavel.g.ro@gmail.com>
2018-05-02 12:42:30 +0300

    Update Rezervare.java

--- Tema2_ISP/src/Rezervare.java
+++ Tema2_ISP/src/Rezervare.java
@@ -14,6 +14,10 @@ public class Rezervare {
 		
 	}
 	
+	//Au, aici e tricky. Cred ca ambele trebuie sa primeasca paremetrii user si eveniment. Nu stiu sigur in ce clasa ar trebui sa fie
+	//metoda, dar cred ca e ok si aici... O sa facen new Rezervare apoi Rezervare.completeazaRezervare(User, Eveniment)
+	//Poti sa mi-i lasi mie... -Cristina
+	
 	public void afisareDetalii() {
 		System.out.println("ID rezervare: " + ID);
 		System.out.println("Detinator: ");
