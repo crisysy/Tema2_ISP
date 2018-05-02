8a833bd543cfb1cc2b0c64fd279de8f502bff084
crisysy <cristinapavel.g.ro@gmail.com>
2018-05-02 14:36:55 +0300

    Revert "Revert "Add files via upload""
    This reverts commit 46a5d07145cb76f16ce7902602701e229760dc98.

---
+++ Tema2_ISP/src/Admin.java
@@ -0,0 +1,18 @@
+
+public class Admin extends Utilizator {
+	//-------------------------------
+	
+	public void adaugaEveniment(Eveniment event) {
+		//ugh...discutie
+		//ar merge singleton la Evenimente
+		//ca asta trebuie sa adauge eveniment in lista cu toate ev
+	}
+	
+	public void stergeEveniment(Eveniment event) {
+		//idem
+	}
+	
+	public void editeazaEveniment(Eveniment event) {
+		//idem
+	}
+}
